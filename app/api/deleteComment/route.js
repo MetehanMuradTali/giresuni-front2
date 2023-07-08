1759b6d046b776d8f72c611d2b8ca79517b124eb
Metehan Murad Tali <56204751+MetehanMuradTali@users.noreply.github.com>
2023-07-08 15:15:56 +0300

    Update route.js

--- app/api/deleteComment/route.js
+++ app/api/deleteComment/route.js
@@ -1,7 +1,13 @@
-import { useAuth } from "../../../hooks/auth"
 import { NextResponse } from "next/server"
 export async function POST(request){
-    const Auth=await useAuth.fromServer()
+   async function fromServer(){
+    const cookieList = cookies();
+
+    const {value:token} = cookieList.get("token") ?? {value:null};
+    const verifiedToken = await verifyJwtToken(token);
+    return verifiedToken;
+}
+const Auth=await fromServer()
     if(Auth){
         if(Auth.role=="admin"){
             const FormData=await request.json()

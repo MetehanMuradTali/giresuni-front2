06f4cf1f6f54bc559bbc476b5c57479d82628da2
Metehan Murad Tali <56204751+MetehanMuradTali@users.noreply.github.com>
2023-07-09 23:45:24 +0300

    Update index.js

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -2,14 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 import styles from './page.module.css'
 const index = ({pageNumber}) => {
-    console.log("1:",pageNumber);
-    pageNumber= parseInt(pageNumber);
-    console.log("2:",pageNumber);
-    
+    let prePage= parseInt(pageNumber)-1
+    let nextPage = parseInt(pageNumber)+1
   return (
     <div className={styles["btn-container"]}>
-        {pageNumber>1 ? <Link href={`/${(pageNumber)-1}`} className={`${styles["pre-btn"]} primary-button`}>Önceki Sayfa</Link> : ""} 
-        <Link href={`/${(pageNumber)+1}`} className={`${styles["next-btn"]} primary-button`}>Sonraki Sayfa</Link>
+        {pageNumber>1 ? <Link href={`/${prePage}`} className={`${styles["pre-btn"]} primary-button`}>Önceki Sayfa</Link> : ""} 
+        <Link href={`/${nextPage}`} className={`${styles["next-btn"]} primary-button`}>Sonraki Sayfa</Link>
     </div>
   )
 }
